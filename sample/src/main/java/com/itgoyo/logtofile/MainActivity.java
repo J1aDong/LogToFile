054c92d1524a57239712d9b1d3ac4316b639ac24
mr.j <991383877@qq.com>
2023-12-05 11:36:11 +0800

    更新1.06

--- sample/src/main/java/com/itgoyo/logtofile/MainActivity.java
+++ sample/src/main/java/com/itgoyo/logtofile/MainActivity.java
@@ -1,7 +1,7 @@
 package com.itgoyo.logtofile;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.itgoyo.logtofilelibrary.LogToFileUtils;
 
